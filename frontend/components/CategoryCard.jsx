09537a9dbdf68e59983fe5a0314e3278e86f27e2
Luis Chilo <gfxargentina@gmail.com>
2022-07-12 19:52:51 -0300

    add cards categories section style

--- frontend/components/CategoryCard.jsx
+++ frontend/components/CategoryCard.jsx
@@ -2,33 +2,20 @@ import React from "react";
 
 import main from "../img/placa-main2.jpg";
 
-const CategoryCard = () => {
+const CategoryCard = ({ item }) => {
   return (
-    <div className="max-w-sm mt-5 mb-5">
-      <a className="relative block bg-black group rounded-lg" href="">
+    <div className="mt-5 mb-5">
+      <div className="max-w-sm relative  rounded-lg shadow-2xl overflow-hidden">
         <img
-          className="absolute inset-0 object-cover w-full h-full transition-opacity opacity-75 group-hover:opacity-50"
-          src="https://res.cloudinary.com/dgeysldss/image/upload/v1656722834/repuestos/zz0tpdhveakdgpk57bx4.jpg"
+          className="object-cover h-52"
+          src={item.img}
           alt="placa main de televisor"
         />
-        <div className="relative p-8">
-          <p className="text-sm font-medium tracking-widest text-pink-500 uppercase">
-            Developer
-          </p>
 
-          <p className="text-2xl font-bold text-white">Placas Main</p>
-
-          <div className="mt-64">
-            <div className="transition-all transform translate-y-8 opacity-0  group-hover:opacity-100 group-hover:translate-y-0">
-              <p className="text-sm text-white">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
-                perferendis hic asperiores quibusdam quidem voluptates
-                doloremque reiciendis nostrum harum. Repudiandae?
-              </p>
-            </div>
-          </div>
-        </div>
-      </a>
+        <h1 className=" absolute font-semibold  text-xl bg-white p-2 rounded-lg bottom-5 left-5">
+          {item.title}
+        </h1>
+      </div>
     </div>
   );
 };
--- frontend/sections/Categories.jsx
+++ frontend/sections/Categories.jsx
@@ -1,10 +1,49 @@
 import React from "react";
 import CategoryCard from "../components/CategoryCard";
 
+const data = [
+  {
+    id: 0,
+    title: "Placas Main",
+    img: "https://res.cloudinary.com/dgeysldss/image/upload/v1656722834/repuestos/zz0tpdhveakdgpk57bx4.jpg",
+  },
+  {
+    id: 1,
+    title: "Fuentes",
+    img: "https://res.cloudinary.com/dgeysldss/image/upload/v1656722834/repuestos/zz0tpdhveakdgpk57bx4.jpg",
+  },
+  {
+    id: 2,
+    title: "Placas T-Con",
+    img: "https://res.cloudinary.com/dgeysldss/image/upload/v1656722834/repuestos/zz0tpdhveakdgpk57bx4.jpg",
+  },
+  {
+    id: 3,
+    title: "Tiras Led",
+    img: "https://res.cloudinary.com/dgeysldss/image/upload/v1656722834/repuestos/zz0tpdhveakdgpk57bx4.jpg",
+  },
+  {
+    id: 4,
+    title: "Displays",
+    img: "https://res.cloudinary.com/dgeysldss/image/upload/v1656722834/repuestos/zz0tpdhveakdgpk57bx4.jpg",
+  },
+  {
+    id: 5,
+    title: "Remotos",
+    img: "https://res.cloudinary.com/dgeysldss/image/upload/v1656722834/repuestos/zz0tpdhveakdgpk57bx4.jpg",
+  },
+];
+
+//TODO: poner link a las imagenes que lleven a cada categoria
+
 const Categories = () => {
   return (
-    <div className="max-w-5xl mx-auto">
-      <CategoryCard />
+    <div className="max-w-5xl mx-auto ">
+      <div className="flex flex-wrap gap-4">
+        {data.map((item) => (
+          <CategoryCard item={item} key={item.id} />
+        ))}
+      </div>
     </div>
   );
 };
