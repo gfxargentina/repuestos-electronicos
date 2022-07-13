627259da6e9cce3e1e415e277b06cb5b8784733c
Luis Chilo <gfxargentina@gmail.com>
2022-07-13 20:52:14 -0300

    add parts card styles

--- frontend/components/PartsCard.jsx
+++ frontend/components/PartsCard.jsx
@@ -1,7 +1,34 @@
 import React from "react";
 
-const PartsCard = () => {
-  return <div>REPUESTOS</div>;
+const PartsCard = (props) => {
+  return (
+    <>
+      <article className="shadow-md bg-white rounded-3xl p-5 mt-5">
+        <img
+          src={props.img}
+          alt=""
+          loading="lazy"
+          className="h-52 w-full object-cover rounded-3xl md:h-80"
+        />
+
+        <article className="flex items-center justify-between">
+          <div className="pt-5">
+            <h1 className="font-bold">{props.title}</h1>
+            <ul>
+              <li>{props.user}</li>
+              <li>creada el 13/07/22</li>
+            </ul>
+          </div>
+
+          <div>
+            <ul className="text-slate-600 text-sm text-right">
+              <li>3 me gusta</li>
+            </ul>
+          </div>
+        </article>
+      </article>
+    </>
+  );
 };
 
 export default PartsCard;
--- frontend/sections/LastParts.jsx
+++ frontend/sections/LastParts.jsx
@@ -4,39 +4,45 @@ import PartsCard from "../components/PartsCard";
 const data = [
   {
     id: 0,
-    title: "Placas Main",
-    img: "https://res.cloudinary.com/dgeysldss/image/upload/v1656722834/repuestos/zz0tpdhveakdgpk57bx4.jpg",
+    user: "Charly Garcia",
+    title: "Main Samsung 32",
+    img: "https://images.unsplash.com/photo-1586920740199-47ce35183cfd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1931&q=80",
   },
   {
     id: 1,
-    title: "Fuentes",
-    img: "https://res.cloudinary.com/dgeysldss/image/upload/v1656722834/repuestos/zz0tpdhveakdgpk57bx4.jpg",
+    user: "Gustavo Cerati",
+    title: "Fuente Sanyo 42",
+    img: "https://images.unsplash.com/photo-1562408590-e32931084e23?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
   },
   {
     id: 2,
-    title: "Placas T-Con",
-    img: "https://res.cloudinary.com/dgeysldss/image/upload/v1656722834/repuestos/zz0tpdhveakdgpk57bx4.jpg",
+    user: "Fabiana Cantilo",
+    title: "T-Con Sansei 40",
+    img: "https://images.unsplash.com/photo-1555664424-778a1e5e1b48?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
   },
   {
     id: 3,
-    title: "Tiras Led",
-    img: "https://res.cloudinary.com/dgeysldss/image/upload/v1656722834/repuestos/zz0tpdhveakdgpk57bx4.jpg",
+    user: "Leon Gieco",
+    title: "Tiras Led Philips 50",
+    img: "https://images.unsplash.com/photo-1586920740280-e7da670f7cb7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80",
   },
   {
     id: 4,
-    title: "Displays",
-    img: "https://res.cloudinary.com/dgeysldss/image/upload/v1656722834/repuestos/zz0tpdhveakdgpk57bx4.jpg",
+    user: "Mercedes Sosa",
+    title: "Display Samsung 43",
+    img: "https://images.unsplash.com/photo-1517077304055-6e89abbf09b0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80",
   },
   {
     id: 5,
-    title: "Remotos",
-    img: "https://res.cloudinary.com/dgeysldss/image/upload/v1656722834/repuestos/zz0tpdhveakdgpk57bx4.jpg",
+    user: "Indio Solari",
+    title: "IR Pioneer 32",
+    img: "https://images.unsplash.com/photo-1517420704952-d9f39e95b43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
   },
 ];
 
 const LastParts = () => {
   return (
-    <section className="container mx-auto px-5">
+    <section className="container mx-auto px-5 mb-5">
       <h1 className="font-bold text-4xl text-center">
         Ultimos Repuestos agregados
       </h1>
