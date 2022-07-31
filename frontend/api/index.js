ef4ce4574d900761d1ca44dd5b2e717ae2f87d88
Luis Chilo <gfxargentina@gmail.com>
2022-07-31 19:16:42 -0300

    ADD: axios interceptors

--- frontend/api/index.js
+++ frontend/api/index.js
@@ -1,5 +1,19 @@
 import axios from 'axios';
 
-const url = 'http://localhost:4001/repuestos';
+const API = axios.create({ baseURL: 'http://localhost:4001' });
 
-export const fetchRepuestos = () => axios.get(url);
+API.interceptors.request.use((req) => {
+  if (localStorage.getItem('profile')) {
+    req.headers.authorization = `bearer ${
+      JSON.parse(localStorage.getItem('profile')).token
+    }`;
+  }
+  return req;
+});
+
+export const signIn = (formData) => API.post('/user/signin', formData);
+export const signUp = (formData) => API.post('/user/signup', formData);
+
+export const fetchRepuestos = () => API.get('/repuestos');
+export const createRepuesto = (nuevoRepuesto) =>
+  API.post('/repuestos', nuevoRepuesto);
