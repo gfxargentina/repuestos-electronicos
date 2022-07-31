2aec80f213f3e5257d56fb0c3faf9d16f8197a46
Luis Chilo <gfxargentina@gmail.com>
2022-07-31 19:21:02 -0300

    ADD: auth logic

--- frontend/store/reducers.js
+++ frontend/store/reducers.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
-import repuestos from './repuestos';
+import authReducer from './authReducer';
+import repuestos from './repuestosReducer';
 
 export default combineReducers({
   repuestos,
+  authReducer,
 });
